abe1418dd2d4f66021dbf0bb8cfa57647e815d62
Ronald Ashri <ronald@istos.it>
2017-07-27 12:24:09 +0200

    Agents can add sensors and associate listeners to sensor events

--- src/Agent.php
+++ src/Agent.php
@@ -1,14 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ronaldashri
- * Date: 21/07/2017
- * Time: 13:56
- */
 
 namespace actsmart\actsmart;
 
 use actsmart\actsmart\Sensors\SensorInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 
 class Agent
@@ -16,7 +11,33 @@ class Agent
     /** @var  SensorInterface */
     protected $sensors;
 
-    public function __construct() {
+    /** @var EventDispatcher */
+    protected $dispatcher;
 
+    public function __construct(EventDispatcher $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+    }
+
+    public function addSensor(SensorInterface $sensor)
+    {
+        $this->sensors[$sensor->getName()] = $sensor;
+    }
+
+    /**
+     * Makes an infostore a listener to sensor events.
+     *
+     * @param $sensor
+     * @param $infostore
+     */
+    public function bindSensorToStore($sensor, $store)
+    {
+        $this->dispatcher->addListener($sensor->getEventName(), array($store, 'store'));
+    }
+
+
+    public function sensorReceive($sensor_name, $message)
+    {
+        return $this->sensors[$sensor_name]->receive($message);
     }
 }
