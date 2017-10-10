a03151c82c6d21d5be0b79379528b064b7c4eb78
Ronald Ashri <ronald@istos.it>
2017-10-10 17:06:23 +0100

    Controllers should have access to sensors as well as actuators

--- src/Controllers/Active/ActiveController.php
+++ src/Controllers/Active/ActiveController.php
@@ -9,9 +9,11 @@
 namespace actsmart\actsmart\Controllers\Active;
 
 use actsmart\actsmart\Agent;
-use actsmart\actsmart\Controllers\ControllerInterface;
-use actsmart\actsmart\Sensors\SensorEvent;
 use actsmart\actsmart\Actuators\ActuatorInterface;
+use actsmart\actsmart\Controllers\ControllerInterface;
+use actsmart\actsmart\Sensors\SensorInterface;
+use actsmart\actsmart\Sensors\SensorEvent;
+
 
 
 class ActiveController implements ControllerInterface
@@ -38,8 +40,18 @@ class ActiveController implements ControllerInterface
 
     }
 
+    public function request(SensorInterface $s = null)
+    {
+
+    }
+
     public function addActuator(ActuatorInterface $actuator)
     {
         $this->actuators[$actuator->getKey()] = $actuator;
+    }
+
+    public function addSensor(SensorInterface $sensor)
+    {
+        $this->sensors[$sensor->getKey()] = $sensor;
     }
 }
