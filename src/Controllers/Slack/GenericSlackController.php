0c73efb923372fa275bdc60de37f0ccc5e0274a1
Ronald Ashri <ronald@istos.it>
2017-10-16 09:46:37 +0200

    Just trying this out - it should be injected.

--- src/Controllers/Slack/GenericSlackController.php
+++ src/Controllers/Slack/GenericSlackController.php
@@ -3,6 +3,7 @@
 namespace actsmart\actsmart\Controllers\Slack;
 
 use actsmart\actsmart\Agent;
+use actsmart\actsmart\Interpreters\NLP\LUISInterpreter;
 use actsmart\actsmart\Sensors\SensorEvent;
 use actsmart\actsmart\Controllers\Active\ActiveController;
 use actsmart\actsmart\Interpreters\Slack\SlackMessageInterpreter;
@@ -20,6 +21,11 @@ class GenericSlackController extends ActiveController
     {
         parent::__construct($agent);
         $this->slack_verification_token = $slack_verification_token;
-        $this->interpreter = new SlackMessageInterpreter();
+
+        $this->interpreter = new LUISInterpreter(
+            'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/',
+            '8e72713a-9308-4a8c-9fd7-948ad4814101',
+            '330937863aef4f7993e50d567e7a9f72'
+        );
     }
 }
