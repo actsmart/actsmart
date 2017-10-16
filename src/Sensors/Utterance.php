11a4650d86ab9aa13c318314fea9a48bf2dfffda
Ronald Ashri <ronald@istos.it>
2017-10-16 09:45:33 +0200

    Utterance interface to help us specialise events

---
+++ src/Sensors/Utterance.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace actsmart\actsmart\Sensors;
+
+/**
+ * Interface Utterance
+ * An utterance is an event that represents a statement that was made in a dialog.
+ *
+ * @package actsmart\actsmart\Sensors
+ */
+interface Utterance
+{
+    public function getUtterance();
+}
