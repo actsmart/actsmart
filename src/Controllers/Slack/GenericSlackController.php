d73213c3c17818c425de4f9b7a22603135dbffa7
Ronald Ashri <ronald@istos.it>
2017-10-23 07:04:52 +0200

    Some notes on how to handle conversations.

--- src/Controllers/Slack/GenericSlackController.php
+++ src/Controllers/Slack/GenericSlackController.php
@@ -14,9 +14,31 @@ class GenericSlackController extends ActiveController
 
     protected $slack_verification_token;
 
-    public function __construct(Agent $agent, $slack_verification_token)
+    /* Time in seconds that a conversation is still considered as ongoing */
+    protected $conversation_timeout;
+
+    public function __construct(Agent $agent, $slack_verification_token, $conversation_timeout)
     {
         parent::__construct($agent);
         $this->slack_verification_token = $slack_verification_token;
+    }
+
+    public function execute(SensorEvent $e = null)
+    {
+        // New message arrives
+        // 1. Determine if part of existing conversation
+        // Conversations are stored in an Amazon Dynamo DB keyed with
+        // <userid><timestamp> - userid is the current user and we are looking for timestamps
+        // that are larger than $current_timestamp - $conversation_timeout
+
+        // 2a. If no new conversation ongoing we need to instantiate a new conversation.
+        // Determine the intention of the opening message and identify a conversation with an
+        // init scene that matches that. If we have a conversation match instantiate conversation
+        // and reply with the next message in the sequence.
+
+        // 2b. If there is an ongoing conversation instantiate that, determine the current scene
+        // the expected next utterance and match that against what was received to understand if we
+        // can reply with the next utterance or we will need to push up to either scene or conversation
+        // context for clarification.
     }
 }
