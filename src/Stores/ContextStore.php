ee0c94a599e7776fdeff68e6b71011a7205291f0
Ronald Ashri <ronald@istos.it>
2017-11-10 19:15:35 +0100

    Standardize context store.

--- src/Stores/ContextStore.php
+++ src/Stores/ContextStore.php
@@ -3,23 +3,38 @@
 namespace actsmart\actsmart\Stores;
 
 use actsmart\actsmart\Actions\ActionEvent;
+use actsmart\actsmart\Utils\ComponentInterface;
+use actsmart\actsmart\Utils\ComponentTrait;
+use actsmart\actsmart\Utils\ListenerInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
 
-class ContextStore
+class ContextStore implements ComponentInterface, ListenerInterface, StoreInterface
 {
+    use ComponentTrait;
+
     private $context_info = [];
 
-    public function __construct()
+    public function listen(GenericEvent $a)
     {
-        //
-    }
-
-    public function store(ActionEvent $a)
-    {
+        if ($a instanceof ActionEvent)
+        {
+            dd($a->getSubject());
+        }
         $this->context_info[$a->getSubject()] = $a->getActionStatus();
     }
 
     public function retrieve($label)
     {
         return $this->context_info[$label];
+    }
+
+    public function getKey()
+    {
+        return 'store.context';
+    }
+
+    public function listensForEvents()
+    {
+        return ['event.action.generic'];
     }
 }
