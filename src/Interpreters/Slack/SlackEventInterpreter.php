6e97d961c11255a5a0f9c9d5f684dd1f5642436c
Ronald Ashri <ronald@istos.it>
2018-05-16 11:27:20 +0200

    SlackEventInterpreter in place. Does not actually interpret events yet.

--- src/Interpreters/Slack/SlackEventInterpreter.php
+++ src/Interpreters/Slack/SlackEventInterpreter.php
@@ -2,11 +2,30 @@
 namespace actsmart\actsmart\Interpreters\Slack;
 
 use actsmart\actsmart\Interpreters\BaseInterpreter;
+use actsmart\actsmart\Utils\ListenerInterface;
+use actsmart\actsmart\Utils\ListenerTrait;
+use Symfony\Component\EventDispatcher\GenericEvent;
 
-class SlackEventInterpreter extends BaseInterpreter
+class SlackEventInterpreter extends BaseInterpreter implements ListenerInterface
 {
+    use ListenerTrait;
 
+    const SLACK_EVENT_INTERPRETER_PRIORITY = 255;
 
+    public function __construct()
+    {
+        $this->setPriority($this::SLACK_EVENT_INTERPRETER_PRIORITY);
+    }
+
+    public function listen(GenericEvent $e)
+    {
+        $this->interpret($e);
+    }
+
+    public function interpret(GenericEvent $e)
+    {
+        // @todo gather relevant info from events and add to ContextStore
+    }
 
     /**
      * @return string
@@ -14,5 +33,10 @@ class SlackEventInterpreter extends BaseInterpreter
     public function getKey()
     {
         return 'interpreter.slack.event';
+    }
+
+    public function listensForEvents()
+    {
+        return ['event.slack.message', 'event.slack.interactive_message'];
     }
 }
