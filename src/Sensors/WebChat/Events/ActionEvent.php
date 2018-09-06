b62aed9e4257fdb3623cd601dbe0dd365c12cd2e
Ronald Ashri <ronald@istos.it>
2018-09-06 12:05:26 +0200

    ActionEvent supports utterances.

--- src/Sensors/WebChat/Events/ActionEvent.php
+++ src/Sensors/WebChat/Events/ActionEvent.php
@@ -2,8 +2,8 @@
 
 namespace actsmart\actsmart\Sensors\WebChat\Events;
 
-use actsmart\actsmart\Actuators\Slack\SlackMessageAttachment;
 use actsmart\actsmart\Sensors\UtteranceEvent;
+use actsmart\actsmart\Utils\Literals;
 
 class ActionEvent extends WebChatEvent implements UtteranceEvent
 {
@@ -11,11 +11,17 @@ class ActionEvent extends WebChatEvent implements UtteranceEvent
 
     private $callback_id = null;
 
+    private $user_id = null;
+
+    private $timestamp = null;
+
     public function __construct($subject, $arguments = [])
     {
         parent::__construct($subject, $arguments = []);
 
         $this->callback_id = $subject->callback_id ?? null;
+        $this->user_id = $subject->author;
+        $this->timestamp = now();
     }
 
     public function getKey()
@@ -25,11 +31,36 @@ class ActionEvent extends WebChatEvent implements UtteranceEvent
 
     public function getUtterance()
     {
-        return $this->getSubject()->data->text;
+        /* @var \Ds\Map */
+        $utterance = new Map();
+        $utterance->put(Literals::TYPE, Literals::WEB_CHAT_ACTION);
+        $utterance->put(Literals::CALLBACK_ID, $this->getCallbackId());
+        $utterance->put(Literals::SOURCE_EVENT, $this);
+        $utterance->put(Literals::UID, $this->user_id);
+        $utterance->put(Literals::TIMESTAMP, $this->timestamp);
+        return $utterance;
     }
 
     public function getCallbackId()
     {
         return $this->callback_id;
     }
+
+    /**
+     * @return null
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @return \Illuminate\Support\Carbon|null
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+
 }
