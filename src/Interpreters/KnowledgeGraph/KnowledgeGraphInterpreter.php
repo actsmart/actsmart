0d78cd6a1fb2d5dcbe51e00afe4ce39b9b2b2521
Ronald Ashri <ronald@istos.it>
2018-09-18 17:25:00 +0100

    Add KG support to actSmart.

---
+++ src/Interpreters/KnowledgeGraph/KnowledgeGraphInterpreter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace actsmart\actsmart\Interpreters\KnowledgeGraph;
+
+use actsmart\actsmart\Interpreters\NLP\NLPAnalysis;
+
+interface KnowledgeGraphInterpreter
+{
+    public function analyse(NLPAnalysis $nlp): KnowledgeGraphObject;
+}
