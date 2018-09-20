9e2abd1e55ded7c7d4d0b708eee8aea33ae44d89
Stuart Haigh <stuarth@greenshootlabs.com>
2018-09-20 17:36:54 +0100

    adds the concept of a WebChatUtterance event that is listened to by the ConversationController events raised by the WebChatSensor are not listened to by the ConversationController anymore Includes a WebChatSensorEventInterpreter that can be hooked up to listen to WebChatSensor Events

--- src/Sensors/WebChat/WebChatSensor.php
+++ src/Sensors/WebChat/WebChatSensor.php
@@ -56,9 +56,13 @@ class WebChatSensor implements SensorInterface, NotifierInterface, ComponentInte
      */
     public function process($message)
     {
-        $event_type = 'message';
-        if (isset($message->callback_id)) {
-            $event_type = 'action';
+        $event_type = WebChatEventCreator::MESSAGE;
+
+        if ($message->type === WebChatEventCreator::LONGTEXT_RESPONSE) {
+            $event_type = WebChatEventCreator::LONGTEXT_RESPONSE;
+
+        } else if (isset($message->callback_id)) {
+            $event_type = WebChatEventCreator::ACTION;
         }
 
         return $this->eventCreator->createEvent($event_type, $message);
