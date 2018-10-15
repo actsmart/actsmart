7f3d155d741a153c0b9dbd027ab4ab1128f508d7
Ronald Ashri <ronald@istos.it>
2018-10-15 17:33:58 +0200

    More empty functions in abstract BaseStore.

--- src/Stores/BaseStore.php
+++ src/Stores/BaseStore.php
@@ -11,6 +11,7 @@ use actsmart\actsmart\Utils\NotifierTrait;use Ds\Map;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
 
 abstract class BaseStore implements LoggerAwareInterface, ComponentInterface, StoreInterface, ListenerInterface, NotifierInterface
 {
@@ -24,11 +25,29 @@ abstract class BaseStore implements LoggerAwareInterface, ComponentInterface, St
     protected $logger;
 
     /**
-     * Stores can listen to events and decide to store that information for access from other components.
+     * Stores can listen to events and decide to store that information for access from other components or
+     * perform other relevant actions.
      *
      * @inheritdoc
      */
     public function listensForEvents()
+    {
+        return [];
+    }
+
+    /**
+     * Implementation of listen function. To be overridden as required.
+     *
+     * @param GenericEvent $e
+     */
+    public function listen(GenericEvent $e)
+    {}
+
+    /**
+     * This is here as a default for stores that do not handle information requests.
+     * @return array|string[]
+     */
+    public function handlesInformationRequests()
     {
         return [];
     }
